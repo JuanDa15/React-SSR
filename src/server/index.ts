caec5cae81bee821e158c2257ce97f8f645b79e5
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-09-13 21:59:17 -0500

    express server setup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,13 @@
-console.log("hola mundo server xd")
+import express, { Express, Request, Response } from "express";
+import { CONFIG } from "./config"
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+  res.send(`<h1>Hello world in route: ${req.url}</h1>`);
+})
+
+
+app.listen(CONFIG.PORT, () => {
+  console.log('Server started in port:' + CONFIG.PORT);
+})
